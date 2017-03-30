804a651b4ac6ecb71e249454835fa7f4e0664d87
Satyam <satyams407@gmail.com>
2017-03-30 15:01:50 +0530

    just added location feature

--- app/src/main/java/satyam/prashant/usict/tcs/aroundyou/MainActivity.java
+++ app/src/main/java/satyam/prashant/usict/tcs/aroundyou/MainActivity.java
@@ -1,5 +1,6 @@
 package satyam.prashant.usict.tcs.aroundyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,7 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +20,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+
         GridView gridView = (GridView) findViewById(R.id.grid_view);
 
         gridView.setAdapter(new MyAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        Toast.makeText(MainActivity.this, "You Clicked at movies ", Toast.LENGTH_SHORT).show();
+                        Intent movi = new Intent(MainActivity.this, Atm.class);
+                        startActivity(movi);
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "You Clicked at plays ", Toast.LENGTH_SHORT).show();
+                       // Intent playsobj = new Intent(MainActivity.this, Plays.class);
+                       // startActivity(playsobj);
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "You Clicked at sports ", Toast.LENGTH_SHORT).show();
+                        //Intent sportsobj = new Intent(MainActivity.this, Sports.class);
+                        //startActivity(sportsobj);
+                        break;
+                    case 3:
+                        Toast.makeText(MainActivity.this, "You Clicked at Events ", Toast.LENGTH_SHORT).show();
+                        //Intent eventsobj= new Intent(MainActivity.this,Events.class);
+                        //startActivity(eventsobj);
+                        break;
+                }
+            }
+        });
     }
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -57,5 +82,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    */
+
 }
