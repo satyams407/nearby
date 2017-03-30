12b20e88659fccb63cd41fc6fa056e2ce14986b1
Satyam <satyams407@gmail.com>
2017-03-31 03:05:03 +0530

    test4

--- app/src/main/java/satyam/prashant/usict/tcs/aroundyou/Mylocation.java
+++ app/src/main/java/satyam/prashant/usict/tcs/aroundyou/Mylocation.java
@@ -1,10 +1,9 @@
 package satyam.prashant.usict.tcs.aroundyou;
 
 import android.Manifest;
-import android.app.AlertDialog;
+
 import android.app.Service;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -12,7 +11,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
